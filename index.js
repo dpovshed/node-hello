9215ded8184c1fe43b315cb8086a3f8e0fe18eee
Dennis Povshedny <basturma@gmail.com>
2025-02-17 22:00:24 +0000

    OS release

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const os = require('os');
+const fs = require('fs');
+
+const osType = os.type(); // e.g., 'Linux'
+const osRelease = os.release(); // e.g., '5.4.0-42-generic'
+let osDistro = 'Unknown';
+if (osType === 'Linux') {
+  try {
+    const osReleaseContent = fs.readFileSync('/etc/os-release', 'utf8');
+    const lines = osReleaseContent.split('\n');
+    for (const line of lines) {
+      if (line.startsWith('PRETTY_NAME=')) {
+        osDistro = line.split('=')[1].replace(/"/g, '');
+        break;
+      }
+    }
+  } catch (err) {
+    console.error('Error reading /etc/os-release:', err);
+  }
+}
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Wow! Hello World!\n'
+  let msg = 'Wow! Hello World!\n'
   const version = process.versions.node
-  res.end(msg + ' Node version: ' + version + '\n');
+  msg += 'Node version: ' + version + '\n'
+  msg += `OS Type: ${osType}\n`;
+  msg += `OS Release: ${osRelease}\n`;
+  msg += osDistro + '\n';
+  res.end(msg);
 });
 
 server.listen(port, () => {
