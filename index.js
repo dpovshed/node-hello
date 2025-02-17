64126cc7173badfba2ea427b0a7708721a26ab18
Dennis Povshedny <basturma@gmail.com>
2025-02-17 21:48:15 +0000

    Tune up misprint

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
-const ver = process.version.node
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello World!\n'
-  res.end(msg + ' Node version: ' + ver + '\n');
+  const msg = 'Wow! Hello World!\n'
+  const version = process.versions.node
+  res.end(msg + ' Node version: ' + version + '\n');
 });
 
 server.listen(port, () => {
